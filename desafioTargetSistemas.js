fd3be3040cea0b4c915a9583f6f014cfc2772b35
Diogo Asenjo <diogoasenjo@outlook.com>
2025-01-10 14:49:46 -0300

    Solução do desafio 02

--- desafioTargetSistemas.js
+++ desafioTargetSistemas.js
@@ -11,3 +11,25 @@ while (k < indice) {
 console.log(soma);
 
 //Resposta: conforme inclusive comprovado pelo código acima, o valor de soma ao final será 91!
+
+//Desafio 02
+function numeroPertenceAFibonacci(numero) {
+  let a = 0;
+  let b = 1;
+  let c;
+
+  if (numero == a || numero == b) return true;
+
+  while (numero >= b) {
+    c = a + b;
+    if (numero == c) return true;
+    a = b;
+    b = c;
+  }
+
+  return false;
+}
+
+//Basta alterar o valor da variável e rodar o arquivo pora saber se o número pertence ou não.
+let numeroQueDesejoSaberSePertenceAFibonacci = 21;
+console.log(numeroPertenceAFibonacci(numeroQueDesejoSaberSePertenceAFibonacci));
